30242e6c119148f7193d51641dafaea1ee80641d
apzaripova <ap.zaripova@gmail.com>
2022-01-25 17:26:52 +0300

    fixed problems

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 const NotAuthError = require('../errors/NotAuthError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 module.exports = (req, res, next) => {
   const token = req.cookies.userToken;
 
@@ -11,7 +13,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'dev-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key');
   } catch (err) {
     next(new NotAuthError('Неверный токен, необходимо авторизоваться'));
   }
