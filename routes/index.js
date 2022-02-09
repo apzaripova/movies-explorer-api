caf2aa4ff758c2005677feb412be1265bdac1530
apzaripova <ap.zaripova@gmail.com>
2022-02-09 19:17:34 +0300

    proverka

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ Router.post('/signin', validateSignIn, login);
 
 Router.delete('/signout', auth, logout);
 
-Router.use('/', auth, usersRouter);
+Router.use('/users', auth, usersRouter);
 Router.use('/', auth, moviesRouter);
 
 Router.use('*', (req, res, next) => {
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const usersRouter = require('express').Router();
 const { getUser, updateUserProfile } = require('../controllers/users');
 const { validateUpdateUserProfile } = require('../middlewares/validation');
 
-usersRouter.get('/users/me', getUser);
-usersRouter.patch('/users/me', validateUpdateUserProfile, updateUserProfile);
+usersRouter.get('/me', getUser);
+usersRouter.patch('/me', validateUpdateUserProfile, updateUserProfile);
 
 module.exports = usersRouter;
