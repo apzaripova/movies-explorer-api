12efe7cc49a3ce4c115982a135accb4669b0a838
apzaripova <ap.zaripova@gmail.com>
2022-03-09 14:27:44 +0300

    fixed

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const validator = require('validator');
+const NotAuthError = require('../errors/NotAuthError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -23,5 +25,23 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function findUser(email, password) {
+  return this.findOne({ email }).select('+password')
+
+    .then((user) => {
+      if (!user) {
+        throw new NotAuthError('Необходима авторизация');
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            throw new NotAuthError('Необходима авторизация');
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
